deddd49518df6b20980a48746a09120537ee8fe0
Doug Ritchie <dritchie3@sbcglobal.net>
2020-03-20 14:30:32 -0500

    add prompt questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,37 @@
-const questions = [
+// add required const.
+const fs = require("fs");
+const generateHTML = require("./generateHTML");// needs {color:}
+console.log("x",generateHTML)
+const inquirer = require("inquirer");
+const path = require("path");
+const open = require("open");//look into
+
+
+
+
+// const questions = [
   
-];
+// ];
 
 function writeToFile(fileName, data) {
  
 }
 
 function init() {
+inquirer.prompt([
+    {message: 'What is your github user name?', 
+    type: 'input',
+    name: 'username'},
+    {
+    type: "list",
+    name: "color",
+    message: "What Color would you like?",
+    choices: ["red", "blue", "green", "pink"]
+    }
+    ])
+.then(ans=>console.log(ans))
 
+
+};
+    
 init();
